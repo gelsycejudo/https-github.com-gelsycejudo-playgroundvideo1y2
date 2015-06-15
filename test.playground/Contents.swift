969a137f1d0410f579d2ace49d9e290058b2743c
gelsycejudo <gelsycejudo@hotmail.com>
2015-06-14 23:53:04 -0500

    video1y2

---
+++ test.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var name = "Ryan"
+var title = "Mr"
+
+var returnValue = "Hello \(title) \(name)"
+
+var strongName:String = "Ryan"
+
+var score = 125
+var multi = 4
+
+var output = "You score is \(score * multi)"
+
+
+
+let PUBLIC_DNS = "http://mywebsite.com";
+
+var teams = [1, 2, 3, 4, 5]
+var team1 = teams [1]
+var team0 = teams [0]
+
+var team3 = ["heros", "monsters", "seekers", "red", "blue"]
+
+for team in team3{
+    println(team3)
+}
+
+//for (var i = 0; i < 3;  ++1) {
+//println(team3[1])
+//}
+
+var fruit = "apple"
+var myfruit = "bananas"
+
+
+if (fruit == myfruit){
+    println("this is true")
+}else{
+    println("this is false")
+}
+
+var fruit1 = "apple"
+var myfruit1 = "apple"
+
+
+if (fruit1 == myfruit1){
+    println("this is true")
+}else{
+    println("this is false")
+}
+
+
+
+
+
+
+
